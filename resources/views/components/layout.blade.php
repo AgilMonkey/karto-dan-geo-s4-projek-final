980230509604c48d959c231c31db01d4996fd093
Aepu <muhammadagilfadilah@gmail.com>
2024-07-01 22:56:27 +0800

    fixed flex bug

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<header class="bg-white">
+<header class="bg-white shadow-md mb-2">
   <div class="mx-auto flex h-16 max-w-screen-fit items-center gap-8 px-4 ">
 
-    <div class="flex flex-1 items-center md:justify-between">
-      <ul class="flex justify-start gap-6 text-sm">
+    <div class="flex flex-1 items-center justify-between">
+      <ul class="flex justify-start gap-6">
         <li>
           <a class="text-gray-500 transition hover:text-gray-500/75" href="/home"> Home </a>
         </li>
@@ -16,7 +16,7 @@
       <div class="self-end gap-4">
         <div class="">
           <a
-            class="block rounded-md bg-red-400 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-red-700"
+            class="block rounded-md bg-red-400 px-5 py-2.5 font-medium text-white transition hover:bg-red-700"
             href="" id="logoutButton"
           >
             Log off
