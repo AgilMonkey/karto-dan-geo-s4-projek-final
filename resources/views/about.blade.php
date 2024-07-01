813e1c2fc46d298cff94d654e4dcbc092cbd8dca
Aepu <muhammadagilfadilah@gmail.com>
2024-07-01 13:01:24 +0800

    Creaet better header

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
 <x-layout>
     <h1>Hi mom</h1>
     <h2>Names agil</h2>
     <h3 style="color: darkred">かいなでの絵師には総じて醜いものの美しさなどと申すことは、わかろうはずがございませぬ。</h3>
     <h3 style="color: darkred; text-align: right">Other painters are such mediocrities, they have no way to recognize the beauty that lies in ugliness.</h3>
 </x-layout>
+</html>
