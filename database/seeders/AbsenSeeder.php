803c5ed91373f9b61585a5d164c9b88dedc5b76d
Aepu <muhammadagilfadilah@gmail.com>
2024-07-02 02:28:59 +0800

    Make cool seed

--- database/seeders/AbsenSeeder.php
+++ database/seeders/AbsenSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use App\Models\Absen;
+use App\Models\User;
 
 class AbsenSeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class AbsenSeeder extends Seeder
     public function run(): void
     {
         Absen::create([
-            'nama' => "agil",
+            'nama' => User::first()->name,
             'waktu' => "2024-07-02 09:30:00",
             'status' => 'Datang'
         ]);
