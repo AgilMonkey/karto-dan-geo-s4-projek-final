2c8525fb437e00c310a18f377f70322e0c3dd7b4
Aepu <muhammadagilfadilah@gmail.com>
2024-07-02 01:38:23 +0800

    Created models

--- database/migrations/2024_07_01_173725_create_absens_table.php
+++ database/migrations/2024_07_01_173725_create_absens_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('absen', function (Blueprint $table) {
+        Schema::create('absens', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->dateTime('waktu');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('absen');
+        Schema::dropIfExists('absens');
     }
 };
